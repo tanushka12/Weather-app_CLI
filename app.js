ab34a81e23d4401b6d0b7cfc8c0dd7363cce1ee5
tanushka12 <tanured@gmail.com>
2017-12-24 02:35:28 +0530

    changes to weather-app

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'address to fetch weather for',
+      string: true
+    }
+})
+.help()
+.alias('help','h')
+.argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20Abode%20Valley%20India',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
+  if(error){
+    console.log('Unable to connect to Google servers');
+  }
+  else if(body.status === 'ZERO_RESULTS'){
+    console.log('Unable to find that address');
+  }
+  else if (body.status ==='OK') {
+
   //console.log(JSON.stringify(body, undefined, 2)); //property argument is undefined / 2 is the no. of spaces
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
   console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+}
 });
